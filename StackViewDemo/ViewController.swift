258ca697ecb387e98deea75930738ac1e10ed6e4
mmmeyers <marsi286@gmail.com>
2015-10-02 11:11:29 -0400

    Stack View tutorial complete

--- StackViewDemo/ViewController.swift
+++ StackViewDemo/ViewController.swift
@@ -14,6 +14,29 @@ class ViewController: UIViewController
     @IBOutlet weak var verticalStackView: UIStackView!
     @IBOutlet weak var horizontalStackView: UIStackView!
     
+    @IBAction func addStar(sender: AnyObject) {
+        let starImgVw:UIImageView = UIImageView(image: UIImage(named: "star"))
+        starImgVw.contentMode = .ScaleAspectFit
+        self.horizontalStackView.addArrangedSubview(starImgVw)
+        UIView.animateWithDuration(0.25, animations: {
+            self.horizontalStackView.layoutIfNeeded()
+        })
+    }
+    
+    @IBAction func removeStar(sender: AnyObject) {
+        let star:UIView? = self.horizontalStackView.arrangedSubviews.last
+        if let aStar = star
+        {
+            self.horizontalStackView.removeArrangedSubview(aStar)
+            aStar.removeFromSuperview()
+            UIView.animateWithDuration(0.25, animations: {
+                self.horizontalStackView.layoutIfNeeded()
+            })
+        }
+    }
+    
+    
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
